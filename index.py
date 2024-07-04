8c51ad3eea0542f86d15ae9b2c503eae82403083
abdulhadi240 <ah912425@gmail.com>
2024-07-04 15:54:44 +0500

    checking azure

--- index.py
+++ index.py
@@ -5,3 +5,7 @@ app : FastAPI = FastAPI()
 @app.get('/')
 def index():
     return {'message': 'Hello world this is azure containers'}   
+
+@app.get('/about')
+def index():
+    return {'message': 'Hello world this is azure containers'}  
